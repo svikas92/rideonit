c20c98012db49f523159075a9f62729f23c2f44a
Vikas <vikas.soni@rentickle.com>
2018-12-23 13:38:39 +0530

    port updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => console.log(`Listening on port ${port}...`));
 
 module.exports = server;
